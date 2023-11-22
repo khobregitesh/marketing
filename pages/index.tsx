fd34d73a976aafc7b6909e30cec753fce75f4f81
khobregitesh <142988423+khobregitesh@users.noreply.github.com>
2023-11-22 14:49:29 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ const Home: NextPage = () => {
 <a href="https://dherb.shop/wp-content/uploads/2023/10/Dherb.Shop_.webp">
   My Great Link
 </a>
+      Hi
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
